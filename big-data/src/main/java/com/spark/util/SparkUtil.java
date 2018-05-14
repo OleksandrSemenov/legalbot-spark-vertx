125bedb789ddcc00eb73cb39829cb3d5044c7147
antrakos <taraszubrei@gmail.com>
2018-05-14 15:41:32 +0300

    Speed up data analysis using cache

--- big-data/src/main/java/com/spark/util/SparkUtil.java
+++ big-data/src/main/java/com/spark/util/SparkUtil.java
@@ -2,6 +2,7 @@ package com.spark.util;
 
 import com.spark.models.FOP;
 import com.spark.models.UO;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.api.java.function.MapFunction;
@@ -9,12 +10,13 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.storage.StorageLevel;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import java.util.stream.StreamSupport;
 
 /**
  * @author Taras Zubrei
@@ -24,25 +26,35 @@ public class SparkUtil {
 
     public static void parseUO(SparkSession session, RedissonClient redissonClient, String path, boolean initial) {
         redisson = redissonClient;
-        Dataset<Row> ds = session.read()
+        JavaRDD<UO> ds = session.read()
                 .format("com.databricks.spark.xml")
                 .option("rootTag", "DATA")
                 .option("rowTag", "RECORD")
                 .option("charset", "windows-1251")
                 .load(path)
-                .select("EDRPOU", "NAME", "SHORT_NAME", "ADDRESS", "BOSS", "KVED", "STAN", "FOUNDERS");
-        ds.toJavaRDD()
+                .select("EDRPOU", "NAME", "SHORT_NAME", "ADDRESS", "BOSS", "KVED", "STAN", "FOUNDERS")
+                .toJavaRDD()
                 .map(UO::fromXml)
-                .groupBy(UO::getId)
-                .filter(tuple -> StreamSupport.stream(tuple._2.spliterator(), true).anyMatch(SparkUtil::isChanged))
-                .foreach(tuple -> {
-                    final RMap<Integer, UO> map = redisson.getMap("uo/" + tuple._1);
-                    final ArrayList<UO> previousData = new ArrayList<>(map.values());
-                    map.delete();
-                    tuple._2.forEach(t -> map.fastPut(t.hashCode(), t));
-                    if (!initial && !previousData.isEmpty())
-                        System.err.println("Id: " + tuple._1 + ". New data: " + map.values() + ". Old data: " + previousData);
-                });
+                .persist(StorageLevel.MEMORY_AND_DISK());
+        if (initial) {
+            ds.foreach(t -> redisson.getMap("uo/" + t.getId()).fastPut(t.hashCode(), t));
+        } else {
+            final List<Long> updatedIds = ds
+                    .filter(SparkUtil::isChanged)
+                    .map(UO::getId)
+                    .collect();
+            ds.filter(t -> updatedIds.contains(t.getId()))
+                    .groupBy(UO::getId)
+                    .foreach(tuple -> {
+                        final RMap<Integer, UO> map = redisson.getMap("uo/" + tuple._1);
+                        final ArrayList<UO> previousData = new ArrayList<>(map.values());
+                        map.delete();
+                        tuple._2.forEach(t -> map.fastPut(t.hashCode(), t));
+                        if (!previousData.isEmpty())
+                            System.err.println("Id: " + tuple._1 + ". New data: " + map.values() + ". Old data: " + previousData);
+                    });
+        }
+        ds.unpersist();
     }
 
     public static void parseFOP(SparkSession session, RedissonClient redissonClient, String path, boolean initial) {
