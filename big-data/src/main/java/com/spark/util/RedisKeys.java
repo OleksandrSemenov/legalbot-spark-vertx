62129eab0444dc2b0f4f7d707264474b36bbc9d4
antrakos <taraszubrei@gmail.com>
2018-05-21 13:33:37 +0300

    Clean up unused code

--- big-data/src/main/java/com/spark/util/RedisKeys.java
+++ big-data/src/main/java/com/spark/util/RedisKeys.java
@@ -5,10 +5,6 @@ package com.spark.util;
  */
 public interface RedisKeys {
     String UFOP_LAST_UPDATE_DATE = "ufop/date";
-    String USER_TEMPlATE = "user/%s";
     String USER_SUBSCRIPTION_TEMPlATE = "user/%s/subscriptions/%s";
-    String USERS = "users";
     String UO_TEMPLATE = "uo/%s";
-    String UO = "uo";
-    String FOP = "fop";
 }
